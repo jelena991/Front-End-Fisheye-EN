178974080935a47a34b9678fc5025a212998a4b9
Jelena <jelena.novakovic991@gmail.com>
2021-12-22 09:41:55 +0100

    retrived data

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -8,9 +8,20 @@ function photographerFactory(data) {
         const img = document.createElement( 'img' );
         img.setAttribute("src", picture)
         const h2 = document.createElement( 'h2' );
+        const h3 = document.createElement( 'h3');
+        const h4 = document.createElement ('h4');
+        const h5 = document.createElement ('h5')
         h2.textContent = name;
+        h3.textContent = tagline;
+        h4.textContent = price;
+        h5.textContent = city;
+
         article.appendChild(img);
         article.appendChild(h2);
+        article.appendChild(h3);
+        article.appendChild(h4);
+        article.appendChild(h5);
+    
         return (article);
     }
     return { name, picture, id, city, country, tagline, price, getUserCardDOM }
